6966b52e0aacd18bc1cddf295595bbdfe4e99a23
Jonas <jonas789@outlook.com>
2023-05-09 09:39:16 +0100

    Criar a calsse para representar uma tabela generica de base de dados,Criar a calss que representa a tabela tipos , criar a class que representa bebidad

--- app/src/main/res/layout/BdGarrafeira.kt
+++ app/src/main/res/layout/BdGarrafeira.kt
@@ -4,10 +4,6 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-private const val NOME_BASE_DADOS = "Garrafeira.db"
-
-private const val VERSÃO_BASE_DADOS = 1
-
 class bdGarrafeira(
     context: Context?,
 ) : SQLiteOpenHelper(context, "Garrafeira.db", null,1) {
