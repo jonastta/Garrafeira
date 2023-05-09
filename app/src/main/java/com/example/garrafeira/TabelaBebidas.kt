42339f79d2c8e1ffb6c77ebb4ae14b98d89d1249
Jonas <jonas789@outlook.com>
2023-05-09 09:57:56 +0100

    Resolvido erro da criação de tabela bebidadas

--- app/src/main/java/com/example/garrafeira/TabelaBebidas.kt
+++ app/src/main/java/com/example/garrafeira/TabelaBebidas.kt
@@ -5,7 +5,7 @@ import android.provider.BaseColumns
 
 class TabelaProvas(db: SQLiteDatabase) : TabelaBD(db, NOME_TABELA) {
     override fun cria() {
-        db.execSQL("CREATE TABLE $NOME_TABELA ($CHAVE_TABELA, nome TEXT NOT NULL, marca TEXT NOT NULL, TEOR_ALCOOLICO BOOLEAN NOT NULL, id_tipos INTEGER NOT NULL), FOREIGN KEY (id_tipos) REFERENCES TIPOS ${TabelaTipos.NOME_TABELA}(${BaseColumns._ID}) ON DELETE RESTRICT")
+        db.execSQL("CREATE TABLE $NOME_TABELA ($CHAVE_TABELA, marca TEXT NOT NULL, TEOR_ALCOOLICO BOOLEAN NOT NULL, id_tipos INTEGER NOT NULL, FOREIGN KEY (id_tipos) REFERENCES TIPOS ${TabelaTipos.NOME_TABELA}(${BaseColumns._ID}) ON DELETE RESTRICT")
     }
 
     companion object {
--- app/src/main/res/layout/BdGarrafeira.kt
+++ app/src/main/res/layout/BdGarrafeira.kt
@@ -3,15 +3,20 @@ package layout
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import com.example.garrafeira.TabelaBD
+import com.example.garrafeira.TabelaProvas
+import com.example.garrafeira.TabelaTipos
 
 class bdGarrafeira(
     context: Context?,
 ) : SQLiteOpenHelper(context, "Garrafeira.db", null,1) {
-    override fun onCreate(p0: SQLiteDatabase?) {
-        TODO("Not yet implemented")
+    override fun onCreate(db: SQLiteDatabase?) {
+        requireNotNull(db)
+        TabelaTipos(db).cria()
+        TabelaProvas(db).cria()
     }
 
-    override fun onUpgrade(p0: SQLiteDatabase?, OldVersion: Int, p2: Int) {
+    override fun onUpgrade(db: SQLiteDatabase?, OldVersion: Int, p2: Int) {
         TODO("Not yet implemented")
     }
 
